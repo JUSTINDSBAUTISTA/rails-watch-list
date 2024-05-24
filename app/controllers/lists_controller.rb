52b48528a6ef0c8dee75f131412974da3ccf11a1
JUSTIN BAUTISTA <justinbautista0921@gmail.com>
2024-05-24 00:48:20 -0400

    Movie Lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,44 @@
 class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.create(list_param)
+    @list.save
+
+    redirect_to list_path(@list)
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+    @list.update(list_param)
+
+    redirect_to list_path(@list)
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+
+    redirect_to lists_path, status: :see_other
+  end
+
+  private
+
+  def list_param
+    params.require(:list).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :bookmarks
+  resources :lists, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
+  resources :movies, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
